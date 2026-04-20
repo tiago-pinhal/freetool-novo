8b3902b20df9b78a53ce278087b34fa62269f1f6
Tiago Pinhal <pinhalcode@gmail.com>
2026-04-19 21:17:15 -0300

    feat: configure Netlify deployment settings, add SEO/PWA assets, and implement Icon component

---
+++ app/components/Icon.vue
@@ -0,0 +1,27 @@
+<template>
+  <img
+    :src="`https://api.iconify.design/${lib}:${name}`"
+    :alt="name"
+    class="icon"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string
+  lib?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  lib: 'heroicons'
+})
+</script>
+
+<style scoped>
+.icon {
+  display: inline-block;
+  width: 1.5em;
+  height: 1.5em;
+  vertical-align: middle;
+}
+</style>
